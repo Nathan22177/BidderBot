6ec28bf8d696a5b505cb994c03bdca86f5124865
nathan22177 <nathanwiilfox@mail.com>
2019-10-19 12:44:53 +0300

    getting rid of testing trough main

--- src/main/java/com/nathan22177/BidderBot/BidderBotApplication.java
+++ src/main/java/com/nathan22177/BidderBot/BidderBotApplication.java
@@ -8,25 +8,6 @@ public class BidderBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BidderBotApplication.class, args);
-
-//		int quantity = 10;
-//		int cash = 10000;
-//		BidderImpl nathan = new BidderImpl(quantity, cash, new NathanStrategy());
-//		BidderImpl user = new BidderImpl(quantity, cash, new UserStrategy());
-//		for (int i = 0; i < 5; i++) {
-//			int nathansBid = nathan.placeBid();
-//			int usersBid = user.placeBid();
-//			nathan.bids(nathansBid, usersBid);
-//			user.bids(usersBid, nathansBid);
-//			System.out.println("Nathan bid: " + nathansBid);
-//			System.out.println(usersBid > nathansBid
-//					? "You've won!"
-//					: usersBid == nathansBid
-//					? "Draw!"
-//					: "Nathan won!");
-//		}
-//		System.out.println("Nathans QU: " + nathan.getAcquiredAmount() + " Nathan's balance: " + nathan.getBalance());
-//		System.out.println("Users QU: " + user.getAcquiredAmount());
 	}
 
 }
