47c6f2671fa35b45e8c2bdd7112f46cb734a2380
nathan22177 <nathanwiilfox@mail.com>
2019-10-19 12:40:07 +0300

    Uncommenting main for tests

--- src/main/java/com/nathan22177/BidderBot/BidderBotApplication.java
+++ src/main/java/com/nathan22177/BidderBot/BidderBotApplication.java
@@ -3,30 +3,34 @@ package com.nathan22177.BidderBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nathan22177.BidderBot.auction.bidder.BidderImpl;
+import com.nathan22177.BidderBot.auction.strategies.ConsolePlayerStrategy;
+import com.nathan22177.BidderBot.auction.strategies.NathanStrategy;
+
 @SpringBootApplication
 public class BidderBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BidderBotApplication.class, args);
 
-//		int quantity = 10;
-//		int cash = 10000;
-//		BidderImpl nathan = new BidderImpl(quantity, cash, new NathanStrategy());
-//		BidderImpl user = new BidderImpl(quantity, cash, new UserStrategy());
-//		for (int i = 0; i < 5; i++) {
-//			int nathansBid = nathan.placeBid();
-//			int usersBid = user.placeBid();
-//			nathan.bids(nathansBid, usersBid);
-//			user.bids(usersBid, nathansBid);
-//			System.out.println("Nathan bid: " + nathansBid);
-//			System.out.println(usersBid > nathansBid
-//					? "You've won!"
-//					: usersBid == nathansBid
-//					? "Draw!"
-//					: "Nathan won!");
-//		}
-//		System.out.println("Nathans QU: " + nathan.getAcquiredAmount() + " Nathan's balance: " + nathan.getBalance());
-//		System.out.println("Users QU: " + user.getAcquiredAmount());
+		int quantity = 10;
+		int cash = 10000;
+		BidderImpl nathan = new BidderImpl(quantity, cash, new NathanStrategy());
+		BidderImpl user = new BidderImpl(quantity, cash, new ConsolePlayerStrategy());
+		for (int i = 0; i < 5; i++) {
+			int nathansBid = nathan.placeBid();
+			int usersBid = user.placeBid();
+			nathan.bids(nathansBid, usersBid);
+			user.bids(usersBid, nathansBid);
+			System.out.println("Nathan bid: " + nathansBid);
+			System.out.println(usersBid > nathansBid
+					? "You've won!"
+					: usersBid == nathansBid
+					? "Draw!"
+					: "Nathan won!");
+		}
+		System.out.println("Nathans QU: " + nathan.getAcquiredAmount() + " Nathan's balance: " + nathan.getBalance());
+		System.out.println("Users QU: " + user.getAcquiredAmount());
 	}
 
 }
